1d20ec565493874d0a54354577d26252ba529d53
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2022-01-21 22:39:33 -0500

    Deleted Node Modules

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const cors = require("cors");
 const transactions = require("./Controllers/transactionsController");
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
