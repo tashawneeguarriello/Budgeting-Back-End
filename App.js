c2d4410ca50c0a6def753f6ec007d7eefb368033
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2022-02-04 22:09:48 -0500

    Add Descriptive Comments

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
+//DEPENDENCIES
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const transactions = require("./Controllers/transactionsController");
 
+//CONFIGURATION
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 8080;
 
+//ROUTES
 app.get("/", (req, res) => {
   res.send("tashawnee is awesome");
 });
